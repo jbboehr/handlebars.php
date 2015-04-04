2f093fd30afb8bc40f6ce4432d29f05af6592669
John Boehr <jbboehr@gmail.com>
2015-04-03 17:12:06 -0700

    Initial binary

---
+++ bin/handlebars.php
@@ -0,0 +1,59 @@
+#!/usr/bin/env php
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$opts = getopt('t:jp', array(
+  'template:',
+  'compile',
+  'lex',
+  'parse',
+));
+
+if( isset($opts['t']) ) {
+    $templateFile = $opts['t'];
+} else if( isset($opts['template']) ) {
+    $templateFile = $opts['template'];
+} else {
+    echo "No input template\n";
+    exit(1);
+}
+if( $templateFile === '-' ) {
+    $template = stream_get_contents(fopen('php://input', 'r'));
+} else {
+    $template = file_get_contents($templateFile);
+}
+
+if( isset($opts['compile']) ) {
+    // compile
+    if( isset($opts['j']) ) {
+        echo json_encode(handlebars_compile($template), constant('JSON_PRETTY_PRINT'));
+    } else if( isset($opts['p']) ) {
+        var_export(handlebars_compile($template));
+    } else {
+        echo handlebars_compile_print($template);
+    }
+} else if( isset($opts['parse']) ) {
+    // parse
+    if( isset($opts['j']) ) {
+        echo json_encode(handlebars_parse($template), constant('JSON_PRETTY_PRINT'));
+    } else if( isset($opts['p']) ) {
+        var_export(handlebars_parse($template));
+    } else {
+        echo handlebars_parse_print($template);
+    }
+} else if( isset($opts['lex']) ) {
+    // lex
+    if( isset($opts['j']) ) {
+        echo json_encode(handlebars_lex($template), constant('JSON_PRETTY_PRINT'));
+    } else if( isset($opts['p']) ) {
+        var_export(handlebars_lex($template));
+    } else {
+        echo handlebars_lex_print($template);
+    }
+} else {
+    // precompile
+    $compileOptions = array();
+    $handlebars = new \Handlebars\Handlebars();
+    echo $handlebars->precompile($template, $compileOptions);
+}
