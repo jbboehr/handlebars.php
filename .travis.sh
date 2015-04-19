96101c4af113195fda7738dbe0c5e44f71b8fad8
John Boehr <jbboehr@gmail.com>
2015-04-18 17:52:27 -0700

    Add travis_retry usage

--- .travis.sh
+++ .travis.sh
@@ -5,18 +5,18 @@
 case "$1" in
 before_install)
     sudo rm -f /etc/apt/sources.list.d/*rabbit*
-    sudo apt-get update -qq
-    sudo apt-get install software-properties-common || sudo apt-get install python-software-properties || true
-    sudo apt-add-repository -y ppa:jbboehr/handlebars
-    sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-    sudo apt-add-repository -y ppa:jbboehr/ppa
-    sudo apt-add-repository -y ppa:mandel/movie-tracker
-    sudo apt-get update -qq
+    sudo travis_retry apt-get update -qq
+    sudo travis_retry apt-get install software-properties-common python-software-properties
+    sudo travis_retry apt-add-repository -y ppa:jbboehr/handlebars
+    sudo travis_retry apt-add-repository -y ppa:ubuntu-toolchain-r/test
+    sudo travis_retry apt-add-repository -y ppa:jbboehr/ppa
+    sudo travis_retry apt-add-repository -y ppa:mandel/movie-tracker
+    sudo travis_retry apt-get update -qq
     ;;
 install)
     if [ "$TRAVIS_PHP_VERSION" != "hhvm" ]; then
-        sudo apt-get install -qq libhandlebars-dev libtalloc-dev
-        git clone https://github.com/jbboehr/php-handlebars.git
+        sudo travis_retry apt-get install -qq libhandlebars-dev libtalloc-dev
+        travis_retry git clone https://github.com/jbboehr/php-handlebars.git
         cd php-handlebars
         phpize
         ./configure
@@ -25,10 +25,10 @@ install)
         echo "extension=handlebars.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
         cd ..
     else
-        sudo apt-get update -qq
-        sudo apt-get install -qq libhandlebars-dev libtalloc-dev hhvm hhvm-dev g++-4.8 gcc-4.8 libboost1.49-dev libgoogle-glog-dev libjemalloc-dev
+        sudo travis_retry apt-get update -qq
+        sudo travis_retry apt-get install -qq libhandlebars-dev libtalloc-dev hhvm hhvm-dev g++-4.8 gcc-4.8 libboost1.49-dev libgoogle-glog-dev libjemalloc-dev
         sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90
-        git clone https://github.com/jbboehr/hhvm-handlebars.git
+        travis_retry git clone https://github.com/jbboehr/hhvm-handlebars.git
         cd hhvm-handlebars
         hphpize
         cmake .
@@ -39,12 +39,12 @@ install)
     fi
     ;;
 before_script)
-    composer install
+    travis_retry composer install
     php generate-tests.php
     ;;
 after_success)
     if [ "$TRAVIS_PHP_VERSION" != "hhvm" ]; then
-        wget https://scrutinizer-ci.com/ocular.phar
+        travis_retry wget https://scrutinizer-ci.com/ocular.phar
         php ocular.phar code-coverage:upload --format=php-clover coverage.clover
     fi
     ;;
