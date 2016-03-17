98a787597e847a71917d1b4e4bf7a9be1d605433
John Boehr <jbboehr@gmail.com>
2016-03-17 16:26:51 -0700

    Run tests without extension

--- .travis.sh
+++ .travis.sh
@@ -33,7 +33,11 @@ install_php_handlebars)
 
 phpunit)
 	if [ "$COVERAGE" = "true" ]; then
-		./vendor/bin/phpunit --coverage-text --coverage-clover=coverage.clover
+		#./vendor/bin/phpunit --coverage-text --coverage-clover=coverage.clover
+		mkdir coverage
+		./vendor/bin/phpunit -dmemory_limit=256M --coverage-php coverage/coverage1.cov
+		php -n -dzend_extension=xdebug.so -dextension=json.so -dmemory_limit=256M ./vendor/bin/phpunit --coverage-php coverage/coverage2.cov
+		./vendor/bin/phpcov merge --clover coverage.clover coverage
 	else
 		./vendor/bin/phpunit
 	fi
